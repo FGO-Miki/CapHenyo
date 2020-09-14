95b8b2c03ef61a37a505a3bea20b152924666554
FGO-Miki <54696833+FGO-Miki@users.noreply.github.com>
2020-09-14 08:47:26 +0800

    Initial commit

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayScene1()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 0);
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Application Has Quit...");
+        Application.Quit();
+    }
+
+}
